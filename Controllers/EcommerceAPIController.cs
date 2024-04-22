3513f35124c670e96c45fded1f0e42d1c99b7a7d
Ajay Karode <Ajay.K_INT1595@accolite.com>
2024-04-22 13:16:01 +0530

    completed

---
+++ Controllers/EcommerceAPIController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using restapi_assignment.Models;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace restapi_assignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EcommerceAPIController : ControllerBase
+    {
+        private readonly ECommerceDbContext context;
+
+        public EcommerceAPIController(ECommerceDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var data = await context.Products.ToListAsync();
+            return Ok(data) ;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            var product = await context.Products.FindAsync(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return product;
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                ReferenceHandler = ReferenceHandler.Preserve // Configure to handle object cycles
+            };
+
+            var new_product = await context.Products.AddAsync(product);
+            await context.SaveChangesAsync();
+            //return Ok(new_product);
+            return Ok(new_product.Entity);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Product>> UpdateProduct(int id, Product product)
+        {
+           if(id != product.ProductId)
+            {
+                return BadRequest();
+            }
+           context.Entry(product).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok(product);
+
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+            var deleteproduct = await context.Products.FindAsync(id);  
+            if(deleteproduct == null)
+            {
+                return NotFound();
+            }
+            context.Products.Remove(deleteproduct);
+            await context.SaveChangesAsync();
+            return Ok();
+
+        }
+    }
+}
---
+++ Models/ECommerceDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace restapi_assignment.Models;
+
+public partial class ECommerceDbContext : DbContext
+{
+    public ECommerceDbContext()
+    {
+    }
+
+    public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+
+        }
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
